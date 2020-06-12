8c15dd6c6f1254f724cc2e413ca9188ef0a2a1bf
Manjot Khurana <manjot@mbconstruction.local>
2020-06-12 11:25:59 -0400

    Updates

---
+++ precache-manifest.86cb3bb3845038e939fe8303a28b596c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb215b2290b155935fd0a8730d4d91e0",
+    "url": "/ureacreation/index.html"
+  },
+  {
+    "revision": "91843071dcd6fbdc2f93",
+    "url": "/ureacreation/static/css/main.510a8cca.chunk.css"
+  },
+  {
+    "revision": "45653baee78f69dd1fd7",
+    "url": "/ureacreation/static/js/2.c5979af9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ureacreation/static/js/2.c5979af9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91843071dcd6fbdc2f93",
+    "url": "/ureacreation/static/js/main.7315ae0f.chunk.js"
+  },
+  {
+    "revision": "70aa796523be602660ae",
+    "url": "/ureacreation/static/js/runtime-main.7c32efdc.js"
+  }
+]);
